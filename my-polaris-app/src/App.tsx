dc160cd5201e69c8c7395e13920190e3d30ca620
Cassia Brooks <cassia.brooks@shopify.com>
2025-04-25 13:46:12 +0100

    Add Polaris inventory prototype

---
+++ my-polaris-app/src/App.tsx
@@ -0,0 +1,227 @@
+import {
+  AppProvider,
+  Page,
+  Frame,
+  Navigation,
+  TopBar,
+  IndexTable,
+  useIndexResourceState,
+  Text,
+  ButtonGroup,
+  Button,
+  Tabs,
+  LegacyCard
+} from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
+import { useState, useCallback } from "react";
+import enTranslations from '@shopify/polaris/locales/en.json';
+
+function App() {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [selected, setSelected] = useState(0);
+
+  const handleSearchChange = useCallback((value: string) => {
+    setSearchValue(value);
+    setIsSearchActive(value.length > 0);
+  }, []);
+
+  const handleSearchResultsDismiss = useCallback(() => {
+    setIsSearchActive(false);
+    setSearchValue('');
+  }, []);
+
+  const searchFieldMarkup = (
+    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+      <TopBar.SearchField
+        onChange={handleSearchChange}
+        value={searchValue}
+        placeholder="Search"
+        showFocusBorder
+      />
+    </div>
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      searchField={searchFieldMarkup}
+      searchResultsVisible={isSearchActive}
+      onSearchResultsDismiss={handleSearchResultsDismiss}
+    />
+  );
+
+  const navigationMarkup = (
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            label: 'Home',
+            selected: true,
+          },
+          {
+            label: 'Orders',
+            badge: '15',
+          },
+          {
+            label: 'Products',
+          },
+          {
+            label: 'Collections',
+          },
+          {
+            label: 'Inventory',
+          },
+        ]}
+      />
+      <Navigation.Section
+        title="Sales channels"
+        items={[
+          {
+            label: 'Online Store',
+          },
+          {
+            label: 'Point of Sale',
+          },
+        ]}
+      />
+      <Navigation.Section
+        title="Apps"
+        items={[
+          {
+            label: 'Email',
+          },
+        ]}
+      />
+    </Navigation>
+  );
+
+  const tabs = [
+    {
+      id: 'all',
+      content: 'All',
+      accessibilityLabel: 'All inventory',
+      panelID: 'all-inventory-content',
+    },
+    {
+      id: 'incoming',
+      content: 'Incoming',
+      panelID: 'incoming-inventory-content',
+    },
+  ];
+
+  const resourceName = {
+    singular: 'inventory item',
+    plural: 'inventory items',
+  };
+
+  const {selectedResources, allResourcesSelected, handleSelectionChange} =
+    useIndexResourceState([]);
+
+  const rowMarkup = [
+    {
+      id: '1',
+      product: 'Tennis Racket Yellow / S',
+      sku: '48052830125',
+      unavailable: '5',
+      committed: '15',
+      available: '20',
+      onHand: '40',
+      incoming: '10',
+    },
+    {
+      id: '2',
+      product: 'Tennis Racket Yellow / M',
+      sku: '48052830125',
+      unavailable: '34',
+      committed: '20',
+      available: '25',
+      onHand: '45',
+      incoming: '5',
+    },
+    {
+      id: '3',
+      product: 'Tennis Racket Yellow / L',
+      sku: '48052830125',
+      unavailable: '0',
+      committed: '30',
+      available: '20',
+      onHand: '50',
+      incoming: '0',
+    },
+  ].map(
+    ({
+      id,
+      product,
+      sku,
+      unavailable,
+      committed,
+      available,
+      onHand,
+      incoming,
+    }) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={Number(id)}
+      >
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {product}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{sku}</IndexTable.Cell>
+        <IndexTable.Cell>{unavailable}</IndexTable.Cell>
+        <IndexTable.Cell>{committed}</IndexTable.Cell>
+        <IndexTable.Cell>{available}</IndexTable.Cell>
+        <IndexTable.Cell>{onHand}</IndexTable.Cell>
+        <IndexTable.Cell>{incoming}</IndexTable.Cell>
+      </IndexTable.Row>
+    ),
+  );
+
+  return (
+    <AppProvider i18n={enTranslations}>
+      <Frame
+        topBar={topBarMarkup}
+        navigation={navigationMarkup}
+      >
+        <Page
+          title="Inventory: New York City"
+          primaryAction={
+            <ButtonGroup>
+              <Button>Stock counts</Button>
+              <Button>Adjustment history</Button>
+              <Button>More actions</Button>
+            </ButtonGroup>
+          }
+        >
+          <LegacyCard>
+            <Tabs tabs={tabs} selected={selected} onSelect={setSelected} />
+            <IndexTable
+              resourceName={resourceName}
+              itemCount={3}
+              selectedItemsCount={
+                allResourcesSelected ? 'All' : selectedResources.length
+              }
+              onSelectionChange={handleSelectionChange}
+              headings={[
+                {title: 'Inventory item'},
+                {title: 'SKU'},
+                {title: 'Unavailable'},
+                {title: 'Committed'},
+                {title: 'Available'},
+                {title: 'On hand'},
+                {title: 'Incoming'},
+              ]}
+            >
+              {rowMarkup}
+            </IndexTable>
+          </LegacyCard>
+        </Page>
+      </Frame>
+    </AppProvider>
+  );
+}
+
+export default App;
