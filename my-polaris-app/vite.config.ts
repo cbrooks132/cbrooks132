0eb53fdc078ad9cc24090def8eb5e0495a456ba6
Cassia Brooks <cassia.brooks@shopify.com>
2025-04-29 14:30:51 +0100

    Update deployment configuration for correct repository path

--- my-polaris-app/vite.config.ts
+++ my-polaris-app/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/polaris-prototype/',
+  base: '/cbrooks-shopify/polaris-prototype/',
 })
